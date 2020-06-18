cd9c508fca12e0c284111a3d7f7640ce5988c6e5
STOYAN KIROV <kirov@STOYANs-MBP.lan>
2020-06-18 23:53:50 +0300

    Refactor

--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/miekg/dns"
-	"github.com/slack-go/slack"
 	"strings"
 	"time"
+
+	"github.com/miekg/dns"
+	"github.com/slack-go/slack"
 )
 
 func main() {
@@ -14,32 +15,45 @@ func main() {
 	webhook := flag.String("webhook", "", "Your Slack webhook URL")
 	flag.Parse()
 
-	if *domain == ""{
+	if *domain == "" {
 		fmt.Println("Error: Must supply a domain")
 		return
 	}
 
-	dns.HandleFunc(".", func (w dns.ResponseWriter, r *dns.Msg) {
+	dns.HandleFunc(".", func(w dns.ResponseWriter, r *dns.Msg) {
 		m := new(dns.Msg)
 		m.SetReply(r)
 		remoteAddr := w.RemoteAddr().String()
 		q1 := r.Question[0]
 		t := time.Now()
 
-		if dns.IsSubDomain(*domain+".", q1.Name) && q1.Name != "ns1."+*domain+"." && q1.Name != "ns2."+*domain+"." && q1.Name != *domain+"." {
-			addrParts := strings.Split(remoteAddr, ":")
+		ns1 := fmt.Sprintf("ns1.%v.", *domain)
+		ns2 := fmt.Sprintf("ns2.%v.", *domain)
+		mainDomain := fmt.Sprintf("%v.", *domain)
 
-			name := fmt.Sprintf("Lookup Query: `%v`", q1.Name)
-			date := fmt.Sprintf("Received At: `%v`", t.Format("Mon Jan _2 15:04:05 2006"))
-			from := fmt.Sprintf("Received From: `%v`", addrParts[0])
-			queryType := fmt.Sprintf("Query Type: `%v`", dns.TypeToString[q1.Qtype])
-
-			message := fmt.Sprintf("*Received DNS interaction:*\n %v \n %v \n %v \n %v \n", date, from, name, queryType)
-			if *webhook != "" {
-				sendSlack(message, *webhook)
-			} else {
-				fmt.Println(message)
+		blacklist := []string{ns1, ns2, mainDomain}
+		for _, item := range blacklist {
+			if strings.ToLower(q1.Name) == item {
+				return
 			}
+		}
+
+		if !dns.IsSubDomain(*domain+".", q1.Name) {
+			return
+		}
+
+		addrParts := strings.Split(remoteAddr, ":")
+
+		name := fmt.Sprintf("Lookup Query: `%v`", q1.Name)
+		date := fmt.Sprintf("Received At: `%v`", t.Format("Mon Jan _2 15:04:05 2006"))
+		from := fmt.Sprintf("Received From: `%v`", addrParts[0])
+		queryType := fmt.Sprintf("Query Type: `%v`", dns.TypeToString[q1.Qtype])
+
+		message := fmt.Sprintf("*Received DNS interaction:*\n %v \n %v \n %v \n %v \n", date, from, name, queryType)
+		if *webhook != "" {
+			sendSlack(message, *webhook)
+		} else {
+			fmt.Println(message)
 		}
 
 		// Server must responsd, because the client keeps making requests
